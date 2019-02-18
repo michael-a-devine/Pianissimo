c1cc7286a58b135dbc75a71cdc242d920a736875
HazemTwair <hazem3033993@gmail.com>
2019-02-18 16:53:53 +0000

    Chapter 11

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,9 +21,25 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from registration.backends.simple.views import RegistrationView
+from django.contrib.auth.views import password_change, password_change_done
+
+
+# Create a new class that redirects the user to the index page,
+#if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     # above maps any URLs starting with rango/ to be handled by the rango application
-    url(r'^admin/', admin.site.urls),   
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/password/change/$', password_change, {'template_name': 'registration/password_change_form.html'}, name='password_change'),
+    url(r'^accounts/password/change/done/$', password_change_done, {'template_name': 'registration/password_change_done.html'}, name='password_change_done'),
+  #  url(r'^accounts/password/change/$', MyRegistrationView.as_view(), name="auth_password_change"),
+  # url(r'^accounts/password/change/done/$', MyRegistrationView.as_view(), name="auth_password_changed"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
