7c89d6edf9cf14fa1b1f33ef780bd2e66919b38b
HazemTwair <46604479+HazemTwair@users.noreply.github.com>
2019-03-01 17:14:30 +0000

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -138,7 +138,6 @@ def get_server_side_cookie(request, cookie, default_val=None):
         val = default_val
     return val
 
-##def visitor_cookie_handler(request, response):
 def visitor_cookie_handler(request):
     # Get the number of visits to the site.
     # We use the COOKIES.get() function to obtain the visits cookie.
@@ -146,7 +145,6 @@ def visitor_cookie_handler(request):
     # If the cookie doesn't exist, then the default value of 1 is used.
     visits = int(request.COOKIES.get('visits', '1'))
 
-##    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
     last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
@@ -154,20 +152,16 @@ def visitor_cookie_handler(request):
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
         # Update the last visit cookie now that we have updated the count
-##        response.set_cookie('last_visit', str(datetime.now()))
         request.session['last_visit'] = str(datetime.now())
     else:
         # Set the last visit cookie
-##        response.set_cookie('last_visit', last_visit_cookie)
         request.session['last_visit'] = last_visit_cookie
 
     # Update/set the visits cookie
-##    response.set_cookie('visits', visits)
     request.session['visits'] = visits
 
 @login_required
 def restricted(request):
-#    return HttpResponse("Since you're logged in, you can see this text!")
     return render(request, 'rango/restricted.html')
     
 def search(request):
