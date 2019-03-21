b0e6c74278d97ff94dc8a124dd002eca591433d9
Nawaf Lawati <2333466A@student.gla.ac.uk>
2019-03-21 15:45:43 +0000

    added genre pages

--- rango/views.py
+++ rango/views.py
@@ -67,24 +67,27 @@ def show_category(request, category_name_slug):
         # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
+        piece_list = Piece.objects.filter(category = category).order_by('-title')
 
         # Retrieve all of the associated pages.
         # Note that filter() will return a list of page objects or an empty list
-        pages = Page.objects.filter(category=category)
+        #pages = Page.objects.filter(category=category)
 
         # Adds our results list to the template context under name pages.
-        context_dict['pages'] = pages
+        #context_dict['pages'] = pages
         # We also add the category object from
         # the database to the context dictionary.
         # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
+        context_dict['piece_list'] = piece_list
 
     except Category.DoesNotExist:
         # We get here if we didn't find the specified category.
         # Don't do anything -
         # the template will display the "no category" message for us.
         context_dict['category'] = None
-        context_dict['pages'] = None
+        context_dict['piece_list'] = None
+        
 
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
